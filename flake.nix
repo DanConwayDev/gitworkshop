6d87b3aec6cf8aca396fedcc341d0619dab71457
DanConwayDev <DanConwayDev@protonmail.com>
2023-09-22 14:33:46 +0000

    build: add gitlint commit-msg hook
    gitlint provides an excellent standard ruleset for conventional
    commits
    
    the commit-msg shell script is default produced by gitlint
    
    Consideration was given to using a tool such as pre-commit-hooks to
    produce git hooks but ultimately using shell scripts reduces
    dependancies and simplifies producing a determinaistic development
    enviornment with nix.

--- flake.nix
+++ flake.nix
@@ -8,19 +8,21 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    gitignore,
-    flake-utils,
-    ...
-  }:
+  outputs =
+    { self
+    , nixpkgs
+    , gitignore
+    , flake-utils
+    , ...
+    }:
     flake-utils.lib.eachDefaultSystem (
-      system: let
+      system:
+      let
         pkgs = nixpkgs.legacyPackages.${system};
         packageJSON = pkgs.lib.importJSON ./package.json;
         gitignoreSource = gitignore.lib.gitignoreSource;
-      in {
+      in
+      {
         packages = rec {
           site-src = pkgs.mkYarnPackage rec {
             name = "${packageJSON.name}-site-${version}";
@@ -36,7 +38,7 @@
 
           default = pkgs.writeShellApplication {
             name = packageJSON.name;
-            runtimeInputs = [site-src pkgs.nodejs];
+            runtimeInputs = [ site-src pkgs.nodejs ];
             text = ''
               node ${site-src}/libexec/${packageJSON.name}/deps/${packageJSON.name}/build
             '';
@@ -44,7 +46,17 @@
         };
 
         devShell = pkgs.mkShell {
-          buildInputs = [pkgs.yarn pkgs.nodejs];
+          buildInputs = [
+            pkgs.gitlint
+            pkgs.nodejs
+            pkgs.yarn
+          ];
+          shellHook = ''
+            # auto-install git hooks
+            dot_git="$(git rev-parse --git-common-dir)"
+            if [[ ! -d "$dot_git/hooks" ]]; then mkdir "$dot_git/hooks"; fi
+            for hook in git_hooks/* ; do ln -sf "$(pwd)/$hook" "$dot_git/hooks/" ; done
+          '';
         };
       }
     );
