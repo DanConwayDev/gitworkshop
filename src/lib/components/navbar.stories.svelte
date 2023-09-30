73d05541eaf4e8650eab32866709450539332827
DanConwayDev <DanConwayDev@protonmail.com>
2023-10-01 00:00:00 +0100

    refactor: use svelte storybook format
    replace plain typescript storybook format with svelte using
    addon-svelte-csf
    
    this requires running test-storybook with --json-index flag. see:
     - https://github.com/storybookjs/addon-svelte-csf/issues/65
     - https://github.com/storybookjs/test-runner/issues/80

---
+++ src/lib/components/navbar.stories.svelte
@@ -0,0 +1,21 @@
+<script lang="ts" context="module">
+    import type { Meta } from "@storybook/svelte";
+    import Navbar from "$lib/components/navbar.svelte";
+    import { Story, Template } from "@storybook/addon-svelte-csf";
+
+    export const meta: Meta<Navbar> = {
+        title: "Navbar",
+        component: Navbar,
+        tags: ["autodocs"],
+    };
+</script>
+
+<Template let:args>
+    <Navbar {...args} />
+</Template>
+
+<Story name="Default" />
+
+<Story name="NoNIP07" args={{ nip07plugin: false }} />
+
+<Story name="NIP07Exists" args={{ nip07plugin: true }} />
