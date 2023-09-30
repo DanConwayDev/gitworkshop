d663869b0f2ac0b6d9e9a8aa74b049bfe7d68c90
DanConwayDev <DanConwayDev@protonmail.com>
2023-10-01 00:00:00 +0100

    build: add visual regression testing
    add image and dom snapshot comparison for storybook components
    
    image snapshot comparison can been flaky and produce many
    false-positives. a higher threshold has been used to mitigate this
    although it may not identify all regressions. DOM snapshots
    supplement images to increase the likelihood of capturing
    regressions.

---
+++ .storybook/test-runner.ts
@@ -0,0 +1,29 @@
+import type { TestRunnerConfig } from "@storybook/test-runner";
+import { toMatchImageSnapshot } from 'jest-image-snapshot';
+
+const customSnapshotsDir = `${process.cwd()}/__snapshots__`;
+
+
+const config: TestRunnerConfig = {
+    setup() {
+        expect.extend({ toMatchImageSnapshot });
+    },
+    async postRender(page, context) {
+        // DOM Snapshot
+        const elementHandler = await page.$('#storybook-root');
+        if (elementHandler) {
+            const innerHTML = await elementHandler.innerHTML();
+            expect(innerHTML).toMatchSnapshot();
+        }
+        // Image Snapshop
+        const image = await page.screenshot();
+        expect(image).toMatchImageSnapshot({
+            customSnapshotsDir,
+            customSnapshotIdentifier: context.id,
+            failureThresholdType: 'percent',
+            failureThreshold: 0.002,
+        });
+    },
+};
+
+export default config;
