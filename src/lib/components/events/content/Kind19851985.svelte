75d649f2b932d3a404c82e058075bb6cb82e39d6
DanConwayDev <DanConwayDev@protonmail.com>
2024-01-18 00:00:00 +0000

    feat(PRPage): display status event
    list status update in timeline

---
+++ src/lib/components/events/content/Kind19851985.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import type { NDKTag } from "@nostr-dev-kit/ndk";
+    import Status from "$lib/components/prs/Status.svelte";
+
+    export let tags: NDKTag[] = [];
+
+    function extractTagContent(name: string): string | undefined {
+        let tag = tags.find((tag) => tag[0] === name);
+        return tag ? tag[1] : undefined;
+    }
+    let status: string | undefined;
+    $: {
+        status = extractTagContent("t");
+    }
+</script>
+
+<div class="">
+    {#if status}
+        set status to <Status {status} />
+    {:else}
+        set status incorrectly
+    {/if}
+</div>
