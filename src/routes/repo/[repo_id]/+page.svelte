3348fe2892d54268b83933cd5c41eb1dd87b160a
DanConwayDev <DanConwayDev@protonmail.com>
2024-01-18 00:00:00 +0000

    feat(RepoPage): display event not found error
    using daisyui alert component

--- src/routes/repo/[repo_id]/+page.svelte
+++ src/routes/repo/[repo_id]/+page.svelte
@@ -9,17 +9,43 @@
     let repo_id = data.repo_id;
 
     ensureSelectedRepo(repo_id);
+
+    let repo_error = false;
+    $: {
+        repo_error =
+            !$selected_repo.loading && $selected_repo.name.length === 0;
+    }
 </script>
 
-<RepoHeader {...$selected_repo} />
+{#if repo_error}
+    <Container>
+        <div role="alert" class="alert alert-error mt-6 w-full max-w-xs m-auto">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="stroke-current shrink-0 h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                ><path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                /></svg
+            >
+            <span>Error! cannot find repository event</span>
+        </div>
+    </Container>
+{:else}
+    <RepoHeader {...$selected_repo} />
 
-<Container>
-    <div class="md:flex mt-2">
-        <div class="md:w-2/3 md:mr-2">
-            <OpenPRs {repo_id} />
+    <Container>
+        <div class="md:flex mt-2">
+            <div class="md:w-2/3 md:mr-2">
+                <OpenPRs {repo_id} />
+            </div>
+            <div class="w-1/3 ml-2 prose hidden md:flex">
+                <RepoDetails {repo_id} />
+            </div>
         </div>
-        <div class="w-1/3 ml-2 prose hidden md:flex">
-            <RepoDetails {repo_id} />
-        </div>
-    </div>
-</Container>
+    </Container>
+{/if}
