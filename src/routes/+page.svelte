a4f2a16e39566f83c382291c5304fb42d1b45f13
DanConwayDev <DanConwayDev@protonmail.com>
2024-01-31 12:07:24 +0000

    chore: bump ngit version to v0.1.2
    to reflect the newly issued version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,24 @@
   import ReposRecent from "$lib/wrappers/ReposRecent.svelte";
 </script>
 
+<div role="alert" class="alert">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="stroke-current shrink-0 h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    ><path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+    /></svg
+  >
+  <div>
+    <h3 class="text-xs prose">Warning Experimental: expect breaking changes</h3>
+  </div>
+</div>
+
 <div class="flex flex-row">
   <div class="hero bg-pr h-[calc(100vh-4rem)] md:basis-1/2">
     <div class="hero-content text-center">
