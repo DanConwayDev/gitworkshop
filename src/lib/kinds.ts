8c769e151fd919d9c93dd3089786b225c9fb9dd9
DanConwayDev <DanConwayDev@protonmail.com>
2024-02-05 15:12:07 +0000

    feat!: use nip34 kinds
    a series of changes must follow to align with nip34 event formats

--- src/lib/kinds.ts
+++ src/lib/kinds.ts
@@ -1,9 +1,9 @@
-export const reply_kind: number = 1
+export const reply_kind: number = 1622
 
 export const pr_status_kind: number = 19851985
 
-export const repo_kind: number = 30317
+export const repo_kind: number = 30617
 
 export const pr_kind: number = 318
 
-export const patch_kind: number = 317
+export const patch_kind: number = 1617
