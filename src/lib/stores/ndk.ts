6db7f402f86cdf798ba27aca171a9117cd3bf008
DanConwayDev <DanConwayDev@protonmail.com>
2024-02-15 08:49:13 +0000

    fix: curate default relays
    to align with ngit-cli commit:
    6a4519b79cd36d1edbb492a3c98e2a3a0b42e0e9
    
    gitworkshop.dev doesnt wait for timeout so purplerelay.com is also
    included

--- src/lib/stores/ndk.ts
+++ src/lib/stores/ndk.ts
@@ -3,23 +3,14 @@ import NDKSvelte from '@nostr-dev-kit/ndk-svelte'
 // export let base_relays = import.meta.env.DEV
 //   ? ["ws://localhost:8080"]
 //   : [
-//       "wss://relayable.org",
-//       "wss://relay.f7z.io",
-//       "wss://relay.damus.io",
-//       "wss://nos.lol",
-//       "wss://nostr.wine/",
-//       "wss://eden.nostr.land/",
-//       "wss://relay.nostr.band/",
-//     ];
 
 export const base_relays = [
-  'wss://relayable.org',
   'wss://relay.f7z.io',
   'wss://relay.damus.io',
   'wss://nos.lol',
-  'wss://nostr.wine/',
-  'wss://eden.nostr.land/',
-  'wss://relay.nostr.band/',
+  'wss://relay.nostr.band',
+  'wss://purplerelay.com', // reliability untested
+  // 'wss://relayable.org', // free but not so reliable
 ]
 
 // TODO: fallback_relays for if profile cannot be found
