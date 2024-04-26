cb78f603f67b3a35f01c4d96b7530a604dff0ccb
DanConwayDev <DanConwayDev@protonmail.com>
2024-04-26 12:24:30 +0100

    feat(issue): move create button
    up the page so it is alway above the fold
    
    closes nostr:nevent1qqs8cky62ry97jpaduja3urau75rr965jlrxswx6fpxa0nff22x2avspp4mhxue69uhkummn9ekx7mqzyquhg9lcgu6dq8pwn04ald504k4qpxde9e5rrlelfc0d2nkgj0k77ndyh60

--- src/routes/repo/[repo_id]/issues/+page.svelte
+++ src/routes/repo/[repo_id]/issues/+page.svelte
@@ -18,48 +18,59 @@
 </script>
 
 <RepoPageWrapper {identifier} selected_tab="issues">
-  <div class="mt-2 border border-base-400">
-    <div class="flex bg-slate-900">
-      <div class="tabs tabs-lifted tabs-xs flex-none p-2">
-        <button
-          role="tab"
-          class="tab"
-          class:opacity-50={status !== proposal_status_open}
-          class:font-bold={status == proposal_status_open}
-          on:click={() => {
-            status = proposal_status_open
-          }}
+  <div class="mt-2 rounded-tr-lg border border-base-400">
+    <div class="flex rounded-r-lg bg-slate-900">
+      <div class="flex-none">
+        <div class="tabs tabs-lifted tabs-xs p-2">
+          <button
+            role="tab"
+            class="tab"
+            class:opacity-50={status !== proposal_status_open}
+            class:font-bold={status == proposal_status_open}
+            on:click={() => {
+              status = proposal_status_open
+            }}
+          >
+            {$issue_summaries.summaries.filter(
+              (s) => s.status === proposal_status_open
+            ).length} Open
+          </button>
+          <button
+            role="tab"
+            class="tab"
+            class:opacity-50={status !== proposal_status_applied}
+            class:font-bold={status == proposal_status_applied}
+            on:click={() => {
+              status = proposal_status_applied
+            }}
+          >
+            {$issue_summaries.summaries.filter(
+              (s) => s.status === proposal_status_applied
+            ).length} Completed
+          </button>
+          <button
+            role="tab"
+            class="tab"
+            class:opacity-50={status !== proposal_status_closed}
+            class:font-bold={status == proposal_status_closed}
+            on:click={() => {
+              status = proposal_status_closed
+            }}
+          >
+            {$issue_summaries.summaries.filter(
+              (s) => s.status === proposal_status_closed
+            ).length} Closed
+          </button>
+        </div>
+      </div>
+      <div class="flex-auto"></div>
+      <div class="flex-none">
+        <a
+          class="btn btn-success btn-sm h-full text-base-400"
+          href="/repo/{identifier}/issues/new"
         >
-          {$issue_summaries.summaries.filter(
-            (s) => s.status === proposal_status_open
-          ).length} Open
-        </button>
-        <button
-          role="tab"
-          class="tab"
-          class:opacity-50={status !== proposal_status_applied}
-          class:font-bold={status == proposal_status_applied}
-          on:click={() => {
-            status = proposal_status_applied
-          }}
-        >
-          {$issue_summaries.summaries.filter(
-            (s) => s.status === proposal_status_applied
-          ).length} Completed
-        </button>
-        <button
-          role="tab"
-          class="tab"
-          class:opacity-50={status !== proposal_status_closed}
-          class:font-bold={status == proposal_status_closed}
-          on:click={() => {
-            status = proposal_status_closed
-          }}
-        >
-          {$issue_summaries.summaries.filter(
-            (s) => s.status === proposal_status_closed
-          ).length} Closed
-        </button>
+          create issue
+        </a>
       </div>
     </div>
     {#if !$issue_summaries.loading && filtered.length === 0}
@@ -73,7 +84,4 @@
       />
     {/if}
   </div>
-  <a class="btn btn-success my-3" href="/repo/{identifier}/issues/new">
-    create issue
-  </a>
 </RepoPageWrapper>
