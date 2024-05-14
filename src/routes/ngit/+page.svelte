da2ca2e2d78ecd01c0a8ef9c1adfdc3da2204d57
DanConwayDev <DanConwayDev@protonmail.com>
2024-05-14 07:41:42 +0100

    chore: bump ngit version to v1.2.1
    to reflect the newly issued version

--- src/routes/ngit/+page.svelte
+++ src/routes/ngit/+page.svelte
@@ -60,18 +60,18 @@
       </p>
       <p>
         <a
-          href="https://github.com/DanConwayDev/ngit-cli/releases/download/v1.2.0/ngit-x86_64-unknown-linux-gnu.tar.gz"
+          href="https://github.com/DanConwayDev/ngit-cli/releases/download/v1.2.1/ngit-x86_64-unknown-linux-gnu.tar.gz"
           class="btn btn-neutral">Linux</a
         >
         <a
-          href="https://github.com/DanConwayDev/ngit-cli/releases/download/v1.2.0/ngit-x86_64-apple-darwin.tar.gz"
+          href="https://github.com/DanConwayDev/ngit-cli/releases/download/v1.2.1/ngit-x86_64-apple-darwin.tar.gz"
           class="btn btn-neutral">Mac</a
         >
         <a
-          href="https://github.com/DanConwayDev/ngit-cli/releases/download/v1.2.0/ngit-x86_64-pc-windows-msvc.zip"
+          href="https://github.com/DanConwayDev/ngit-cli/releases/download/v1.2.1/ngit-x86_64-pc-windows-msvc.zip"
           class="btn btn-neutral">Windows</a
         >
-        v1.2.0
+        v1.2.1
       </p>
 
       <p>
