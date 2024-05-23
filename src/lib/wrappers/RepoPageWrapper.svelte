bf4168938d00a2297c81452856dff2a802572a35
DanConwayDev <DanConwayDev@protonmail.com>
2024-05-23 10:23:26 +0100

    fix(RepoPage): display error when naddr not found
    previously it would just display as untitled

--- src/lib/wrappers/RepoPageWrapper.svelte
+++ src/lib/wrappers/RepoPageWrapper.svelte
@@ -39,26 +39,32 @@
   }, 5000)
 </script>
 
-{#if invalid_naddr || (waited_5_secs && $selected_repo_collection.loading && $selected_repo_event.name.length)}
+{#if invalid_naddr || (waited_5_secs && !$selected_repo_collection.loading && $selected_repo_event.name.length === 0)}
   <Container>
-    <div role="alert" class="alert alert-error m-auto mt-6 w-full max-w-xs">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-6 w-6 shrink-0 stroke-current"
-        fill="none"
-        viewBox="0 0 24 24"
-        ><path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-        /></svg
-      >
-      {#if invalid_naddr}
-        <span>Error! invalid naddr in url: {repo_naddr}</span>
-      {:else}
-        <span>Error! cannot find repository event: {repo_naddr}</span>
-      {/if}
+    <div role="alert" class="max-w-xl m-auto">
+      <div role="alert" class="alert alert-error m-auto mt-6 break-all">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6 shrink-0 stroke-current"
+          fill="none"
+          viewBox="0 0 24 24"
+          ><path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+          /></svg
+        >
+        <div>
+        {#if invalid_naddr}
+          <div>Error! invalid naddr in url:</div>
+          <div class="break-all">{repo_naddr}</div>
+        {:else}
+          <div>Error! cannot find repository event:</div>
+          <div class="break-all">{repo_naddr}</div>
+        {/if}
+        </div>
+      </div>
     </div>
   </Container>
 {:else}
