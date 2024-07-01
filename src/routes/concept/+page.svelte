db1b3c0daf6ce9a381c3fcfd6eff998677ebcf19
DanConwayDev <DanConwayDev@protonmail.com>
2024-07-01 07:11:31 +0100

    add concept quick start page
    outling the target user flow we are aiming for

---
+++ src/routes/concept/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import AlertError from "$lib/components/AlertError.svelte"
+  import Container from "$lib/components/Container.svelte"
+  import SvelteMarkdown from 'svelte-markdown'
+
+  async function get_md() {
+      const res = await fetch("/concept.md");
+      return await res.text()
+  }
+</script>
+
+<svelte:head>
+  <title>GitWorkshop: Concept</title>
+</svelte:head>
+
+<Container>
+  {#await get_md()}
+    <p>loading...</p>
+  {:then md}
+    <article class="prose prose-sm mt-3">
+      <SvelteMarkdown
+        options={{ gfm: true }}
+        source={md}
+      />
+    </article>
+  {:catch error}
+    <AlertError>
+      {error.message}
+    </AlertError>
+  {/await}
+</Container>
