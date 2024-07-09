a7608151bd27bef54647755449c14ece0dadfe51
DanConwayDev <DanConwayDev@protonmail.com>
2024-07-09 08:46:08 +0100

    chore: add ngit version to v1.3-beta1
    as an additional release along side the stable release

--- src/routes/ngit/+page.svelte
+++ src/routes/ngit/+page.svelte
@@ -90,6 +90,31 @@
         >
       </p>
     </div>
+      <p>
+        Or install the pre-release 
+      </p>
+      <p>
+        <a
+          href="https://github.com/DanConwayDev/ngit-cli/releases/download/v1.3-beta1/ngit-x86_64-unknown-linux-gnu.tar.gz"
+          class="btn btn-neutral">Linux</a
+        >
+        <a
+          href="https://github.com/DanConwayDev/ngit-cli/releases/download/v1.3-beta1/ngit-x86_64-apple-darwin.tar.gz"
+          class="btn btn-neutral">Mac</a
+        >
+        <a
+          href="https://github.com/DanConwayDev/ngit-cli/releases/download/v1.3-beta1/ngit-x86_64-pc-windows-msvc.zip"
+          class="btn btn-neutral">Windows</a
+        >
+        v1.3-beta1
+      </p>
+
+      <p>
+        alternatively, if you have cargo installed run<code
+          >cargo install ngit</code
+        >
+      </p>
+    </div>
     <h3>Commands</h3>
     <p>run from the your product's git repository:</p>
     <p>
