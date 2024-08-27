55a0247d33569c6a1a6b334300e48bae146eee07
DanConwayDev <DanConwayDev@protonmail.com>
2024-08-27 19:50:56 +0100

    chore: bump ngit version to v1.4.4
    to reflect the newly issued version

--- src/lib/components/InstallNgit.svelte
+++ src/lib/components/InstallNgit.svelte
@@ -9,21 +9,21 @@
   </p>
   <p>
     <a
-      href="https://github.com/DanConwayDev/ngit-cli/releases/download/v1.4.3/ngit-x86_64-unknown-linux-gnu.tar.gz"
+      href="https://github.com/DanConwayDev/ngit-cli/releases/download/v1.4.4/ngit-x86_64-unknown-linux-gnu.tar.gz"
       class="btn btn-neutral"
       class:btn-sm={size === 'sm'}>Linux</a
     >
     <a
-      href="https://github.com/DanConwayDev/ngit-cli/releases/download/v1.4.3/ngit-aarch64-apple-darwin.tar.gz"
+      href="https://github.com/DanConwayDev/ngit-cli/releases/download/v1.4.4/ngit-aarch64-apple-darwin.tar.gz"
       class="btn btn-neutral"
       class:btn-sm={size === 'sm'}>Mac</a
     >
     <a
-      href="https://github.com/DanConwayDev/ngit-cli/releases/download/v1.4.3/ngit-x86_64-pc-windows-msvc.zip"
+      href="https://github.com/DanConwayDev/ngit-cli/releases/download/v1.4.4/ngit-x86_64-pc-windows-msvc.zip"
       class="btn btn-neutral"
       class:btn-sm={size === 'sm'}>Windows</a
     >
-    v1.4.3
+    v1.4.4
   </p>
   <p>
     alternatively, if you have cargo installed run<code>cargo install ngit</code
