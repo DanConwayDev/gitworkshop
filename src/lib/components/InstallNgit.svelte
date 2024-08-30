367a1cbd1fd92292af6a53dc279af744cd499870
DanConwayDev <DanConwayDev@protonmail.com>
2024-08-30 14:27:00 +0100

    chore: bump ngit version to v1.4.5
    to reflect the newly issued version

--- src/lib/components/InstallNgit.svelte
+++ src/lib/components/InstallNgit.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   export let size: 'sm' | 'md' = 'md'
+  const version = 'v1.4.5'
 </script>
 
 <div class="prose" class:text-sm={size === 'sm'}>
@@ -9,21 +10,21 @@
   </p>
   <p>
     <a
-      href="https://github.com/DanConwayDev/ngit-cli/releases/download/v1.4.4/ngit-v1.4.4-x86_64-unknown-linux-gnu.tar.gz"
+      href="https://github.com/DanConwayDev/ngit-cli/releases/download/{version}/ngit-{version}-x86_64-unknown-linux-gnu.tar.gz"
       class="btn btn-neutral"
       class:btn-sm={size === 'sm'}>Linux</a
     >
     <a
-      href="https://github.com/DanConwayDev/ngit-cli/releases/download/v1.4.4/ngit-v1.4.4-aarch64-apple-darwin.tar.gz"
+      href="https://github.com/DanConwayDev/ngit-cli/releases/download/{version}/ngit-{version}-aarch64-apple-darwin.tar.gz"
       class="btn btn-neutral"
       class:btn-sm={size === 'sm'}>Mac</a
     >
     <a
-      href="https://github.com/DanConwayDev/ngit-cli/releases/download/v1.4.4/ngit-v1.4.4-x86_64-pc-windows-msvc.zip"
+      href="https://github.com/DanConwayDev/ngit-cli/releases/download/{version}/ngit-{version}-x86_64-pc-windows-msvc.zip"
       class="btn btn-neutral"
       class:btn-sm={size === 'sm'}>Windows</a
     >
-    v1.4.4
+    {version}
   </p>
   <p>
     alternatively, if you have cargo installed run<code>cargo install ngit</code
