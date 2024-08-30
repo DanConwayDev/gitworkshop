111a4c9e1abae134f8b2af73b7ba98dce4a4d5c5
DanConwayDev <DanConwayDev@protonmail.com>
2024-08-30 21:49:23 +0100

    build: use csr build using adapter-static
    and removing adapter-netlify in an effort to reduce server render invocations

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,17 @@
 import { vitePreprocess } from '@sveltejs/kit/vite'
-import adapter from '@sveltejs/adapter-netlify'
+import adapter from '@sveltejs/adapter-static'
 
 const config = {
   preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter(),
+  adapter: adapter({
+    // default options are shown. You can customize them as needed.
+    pages: 'build',
+    assets: 'build',
+    fallback: null, // Set to null to avoid using a fallback HTML file
+  }),
+  prerender: {
+    // Set this to false to disable prerendering
+    default: false,
   },
 }
 
