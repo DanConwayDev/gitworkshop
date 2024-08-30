ab46915e6b880c9274a0b1e7f8af216dd7f27779
DanConwayDev <DanConwayDev@protonmail.com>
2024-08-30 21:23:50 +0100

    build: clean up svelte adapter code
    as part of exploring how to reduce invocations
    of render which seem exccessive

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
-import adapter from '@sveltejs/adapter-netlify';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/kit/vite'
+import adapter from '@sveltejs/adapter-netlify'
 
 const config = {
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(),
+  },
+}
 
-	kit: {
-		adapter: adapter()
-	}
-};
-
-export default config;
+export default config
