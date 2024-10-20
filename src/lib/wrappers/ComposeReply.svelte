805a617124e6c47e4191df4ee0ed3b8a43608c49
DanConwayDev <DanConwayDev@protonmail.com>
2024-10-20 23:48:55 +0100

    refactor: start to replace ndk in composereply
    but without having implemented  outbox, publish or signer

--- src/lib/wrappers/ComposeReply.svelte
+++ src/lib/wrappers/ComposeReply.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-  import { ndk } from '$lib/stores/ndk'
-  import { NDKEvent, NDKRelaySet } from '@nostr-dev-kit/ndk'
   import { type Event } from 'nostr-tools'
   import { reply_kind } from '$lib/kinds'
   import { logged_in_user } from '$lib/stores/users'
@@ -19,7 +17,7 @@
   export let event: Event
   export let sentFunction = () => {}
   let repo_identifier: string
-  let selected_proposal_or_issue: IssueOrPrWithReferences
+  let selected_proposal_or_issue: IssueOrPrWithReferences | undefined
 
   let submitting = false
   let submitted = false
@@ -31,40 +29,40 @@
       type === 'proposal' ? $selected_proposal : $selected_issue
 
     edit_mode =
-      repo_identifier.length > 0 &&
-      selected_proposal_or_issue.summary.id.length > 0 &&
-      !submitted
+      repo_identifier.length > 0 && !!selected_proposal_or_issue && !submitted
   }
   /** to get the proposal revision id rather than the root proposal */
-  const getRootId = (event: Event): string => {
+  const getRootId = (event: Event): string | undefined => {
     // exclude 'a' references to repo events
     let root_tag = event.tags.find(
       (t) => t[0] === 'e' && t.length === 4 && t[3] === 'root'
     )
     if (root_tag) return root_tag[1]
     if (event.tags.some((t) => t[0] === 't' && t[1] === 'root')) return event.id
-    return selected_proposal_or_issue.summary.id
+    return selected_proposal_or_issue
+      ? selected_proposal_or_issue.uuid
+      : undefined
   }
 
   async function sendReply(content: string) {
     if (!$logged_in_user) return
-    let new_event = new NDKEvent(ndk)
-    new_event.kind = reply_kind
-    if (reply_kind !== 1) event.tags.push(['alt', `git reply`])
+    let tags = []
+    if (reply_kind !== 1) tags.push(['alt', `git reply`])
     let relay_hint =
       !$selected_repo_collection || !$selected_repo_collection.relays
         ? ''
         : $selected_repo_collection.relays[0] || ''
-    new_event.tags.push(['e', getRootId(event), relay_hint, 'root'])
+    let root_id = getRootId(event)
+    if (root_id) tags.push(['e', root_id, relay_hint, 'root'])
     if (event.id.length > 0) {
-      new_event.tags.push(['e', event.id, relay_hint, 'reply'])
+      tags.push(['e', event.id, relay_hint, 'reply'])
     }
     if (
       $selected_repo_collection &&
       'unique_commit' in $selected_repo_collection &&
       !!$selected_repo_collection.unique_commit
     ) {
-      new_event.tags.push(['r', $selected_repo_collection.unique_commit])
+      tags.push(['r', $selected_repo_collection.unique_commit])
     }
 
     let parent_event_user_info = await relays_manager.awaitPubKeyInfo(
@@ -72,7 +70,7 @@
     )
 
     if (event.pubkey !== $logged_in_user?.pubkey)
-      new_event.tags.push(
+      tags.push(
         parent_event_user_info.relays.write[0]
           ? ['p', event.pubkey, parent_event_user_info.relays.write[0]]
           : ['p', event.pubkey]
@@ -82,18 +80,23 @@
       .forEach((tag) => {
         if (
           // not duplicate
-          !new_event.tags.some((t) => t[1] === tag[1]) &&
+          !tags.some((t) => t[1] === tag[1]) &&
           // not current user (dont tag self)
           tag[1] !== $logged_in_user?.pubkey
         )
-          new_event.tags.push(tag)
+          tags.push(tag)
       })
-    tagRepoAnns(new_event, $selected_repo_collection)
-    new_event.content = content
+    if ($selected_repo_collection) tagRepoAnns(tags, $selected_repo_collection)
+    let new_event: Partial<Event> = {
+      kind: reply_kind,
+      content,
+      tags: [],
+    }
     submitting = true
+
     let relays = selectedRepoCollectionToRelays($selected_repo_collection)
     try {
-      new_event.sign()
+      // new_event.sign()
     } catch {
       alert('failed to sign event')
     }
@@ -108,9 +111,9 @@
     } catch {}
     // TODO root event user relays
     try {
-      let _ = await new_event.publish(
-        NDKRelaySet.fromRelayUrls([...new Set(relays)], ndk)
-      )
+      // let _ = await new_event.publish(
+      //   NDKRelaySet.fromRelayUrls([...new Set(relays)], ndk)
+      // )
       submitting = false
       submitted = true
       setTimeout(() => {
