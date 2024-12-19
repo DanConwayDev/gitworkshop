805c547cbfbf239f125e9c09c656683208f46438
DanConwayDev <DanConwayDev@protonmail.com>
2024-12-19 13:51:40 +0000

    initial commit nix and svelte boilerplate
    reset from new boilerplate
    
    using npx sv create with setting:
    - mininal
    - typescript
    - prettier
    - eslint
    - vitest
    - tailwind (with typography and container-queries plugin)
    - static adapter
    - pnpm

--- flake.nix
+++ flake.nix
@@ -2,49 +2,33 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    gitignore = {
-      url = "github:hercules-ci/gitignore.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    gitignore,
-    flake-utils,
-    ...
-  }:
+  outputs =
+    { nixpkgs
+    , flake-utils
+    , ...
+    }:
     flake-utils.lib.eachDefaultSystem (
-      system: let
+      system:
+      let
         pkgs = nixpkgs.legacyPackages.${system};
-        packageJSON = pkgs.lib.importJSON ./package.json;
-        gitignoreSource = gitignore.lib.gitignoreSource;
-      in {
-        packages = rec {
-          site-src = pkgs.mkYarnPackage rec {
-            name = "${packageJSON.name}-site-${version}";
-            version = packageJSON.version;
-            src = gitignoreSource ./.;
-            packageJson = "${src}/package.json";
-            yarnLock = "${src}/yarn.lock";
-            buildPhase = ''
-              yarn --offline build
-            '';
-            distPhase = "true";
-          };
-
-          default = pkgs.writeShellApplication {
-            name = packageJSON.name;
-            runtimeInputs = [site-src pkgs.nodejs];
-            text = ''
-              node ${site-src}/libexec/${packageJSON.name}/deps/${packageJSON.name}/build
-            '';
-          };
-        };
+      in
+      {
 
         devShell = pkgs.mkShell {
-          buildInputs = [pkgs.yarn pkgs.nodejs];
+          buildInputs = [
+            pkgs.gitlint
+            pkgs.nodejs
+            pkgs.pnpm
+          ];
+
+          shellHook = ''
+            # auto-install git hooks
+            dot_git="$(git rev-parse --git-common-dir)"
+            if [[ ! -d "$dot_git/hooks" ]]; then mkdir "$dot_git/hooks"; fi
+            for hook in git_hooks/* ; do ln -sf "$(pwd)/$hook" "$dot_git/hooks/" ; done
+          '';
         };
       }
     );
