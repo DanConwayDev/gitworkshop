07d386fd7d36e4ac3aacd0575e3bb58e79ef50fc
DanConwayDev <DanConwayDev@protonmail.com>
2025-01-06 16:31:12 +0000

    feat: make PWA via vite-pwa
    which has SvelteKit support

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			registerType: 'autoUpdate',
+			manifestFilename: 'manifest.json'
+		})
+	],
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
