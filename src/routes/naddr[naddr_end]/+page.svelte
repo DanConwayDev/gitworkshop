2fda259b16910f03416aac5de9859d5efe934d34
DanConwayDev <DanConwayDev@protonmail.com>
2025-01-10 17:21:22 +0000

    add naddr pages
    this should have been included some commits ago.

---
+++ src/routes/naddr[naddr_end]/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import RepoPage from '$lib/components/repo/RepoPageContainer.svelte';
+	import { naddrToRepoA } from '$lib/utils';
+
+	export let data: { repo_naddr: string };
+	let repo_naddr = data.repo_naddr;
+
+	let a_ref = naddrToRepoA(repo_naddr);
+</script>
+
+{#if a_ref}
+	<RepoPage {a_ref} />
+{/if}
