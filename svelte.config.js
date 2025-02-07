0a8d08097230a9747565dde0ee8f0090e7456b5c
DanConwayDev <DanConwayDev@protonmail.com>
2025-02-07 09:35:18 +0000

    build: use netlify svelte adapter
    apparently this is the setup for statically build sites on netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,7 +12,9 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: 'app.html'
+			fallback: 'app.html',
+			edge: false,
+			split: false
 		})
 	}
 };
