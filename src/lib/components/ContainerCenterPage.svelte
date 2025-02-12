9cebe22525e57ec71e3852ea5f4d8ea602d679c0
DanConwayDev <DanConwayDev@protonmail.com>
2025-02-12 16:10:19 +0000

    add footer
    as previously reccommended

--- src/lib/components/ContainerCenterPage.svelte
+++ src/lib/components/ContainerCenterPage.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	// appears in the centre of the screen adapting for hardcoded Navbar and Footer heights
+	// negative margin 24 matches the margin of the footer
 </script>
 
-<div class="flex items-center justify-center h-screen center-of-gw-screen">
+<div class="flex items-center justify-center h-screen center-of-gw-screen -mb-24">
     <slot />
 </div>
 
@@ -12,7 +13,7 @@
         flex-direction: column;
         align-items: center;
         justify-content: center;
-        height: calc(100vh - 110px);
+        height: calc(100vh - 130px);
         text-align: center;
     }
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,11 +4,40 @@
 
 	import Navbar from '$lib/components/Navbar.svelte';
 	import '../app.css';
+	import Container from '$lib/components/Container.svelte';
 	let { children } = $props();
 </script>
 
-<Navbar />
+<style>
+    .gw-page-container {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh; /* Ensure the container takes at least the full height of the viewport */
+    }
+    .gw-main {
+        flex-grow: 1; /* Allow the main content to grow and fill the available space */
+    }
+</style>
 
-{@render children()}
+<div class="gw-page-container">
+	<Navbar />
+    
+	<main class="gw-main">
+		{@render children()}
+    </main>
 
-<div class="h-10"></div>
+    <footer class="bg-base-200 mt-24">
+		<div class="flex my-3">
+			<div class="flex-grow"></div>
+			<div class="flex items-center">
+				<div class="h-10 text-center text-xs text-neutral-content flex items-center">
+					<a class="inline" href="/">
+						<img src="/icons/icon.svg" alt="gitworkshop.dev logo" class="h-5 w-5 inline" />
+					</a>
+					<span class="ml-2 mt-0.5">GitWorkshop.dev</span>
+				</div>
+			</div>
+			<div class="flex-grow"></div>
+		</div>
+    </footer>
+</div>
