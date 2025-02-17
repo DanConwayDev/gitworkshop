99abe7c03496a6126dc2c8a3e0abcbcfa41ed2db
DanConwayDev <DanConwayDev@protonmail.com>
2025-02-17 12:05:07 +0000

    chore: formatting with prettier

--- src/lib/query-centre/QueryCentre.svelte.ts
+++ src/lib/query-centre/QueryCentre.svelte.ts
@@ -118,7 +118,9 @@ class QueryCentre {
 	}
 
 	fetchIssues(a_ref: RepoRef) {
-		return liveQueryState(() => db.issues.where('repos').equals(a_ref).reverse().sortBy('last_activity',));
+		return liveQueryState(() =>
+			db.issues.where('repos').equals(a_ref).reverse().sortBy('last_activity')
+		);
 	}
 
 	fetchIssue(issue_id: EventIdString) {
@@ -132,7 +134,9 @@ class QueryCentre {
 	}
 
 	fetchPrs(a_ref: RepoRef) {
-		return liveQueryState(() => db.prs.where('repos').equals(a_ref).reverse().sortBy('last_activity'));
+		return liveQueryState(() =>
+			db.prs.where('repos').equals(a_ref).reverse().sortBy('last_activity')
+		);
 	}
 
 	fetchPr(pr_id: EventIdString) {
