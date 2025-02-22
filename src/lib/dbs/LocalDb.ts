35d9a39c0ad38beb89aec7857afdf4cb0739c089
DanConwayDev <DanConwayDev@protonmail.com>
2025-02-22 13:51:53 +0000

    fix: db schema upgrade for `status_history`
    and pick up deleltions

--- src/lib/dbs/LocalDb.ts
+++ src/lib/dbs/LocalDb.ts
@@ -1,5 +1,6 @@
 import Dexie from 'dexie';
-import type { LocalDbSchema, RepoTableItem } from '$lib/types';
+import { IssueOrPrStatus, type LocalDbSchema, type RepoTableItem } from '$lib/types';
+import { clearLocalRelayDb } from './LocalRelayDb';
 
 const db = new Dexie('localdb') as Dexie & LocalDbSchema;
 
@@ -11,6 +12,54 @@ db.version(1).stores({
 	last_checks: '&url_and_query',
 	outbox: '&event.id'
 });
+
+db.version(2)
+	.stores({
+		repos: '&uuid, identifier, author, *searchWords',
+		issues: '&uuid, parent_ids, *repos',
+		prs: '&uuid, parent_ids, *repos',
+		pubkeys: '&pubkey, *verified_nip05',
+		last_checks: '&url_and_query',
+		outbox: '&event.id'
+	})
+	.upgrade(async (tx) => {
+		// added deletion support
+		// added uuid to status_history
+		await Promise.all([
+			tx
+				.table('issues')
+				.toCollection()
+				.modify((issue) => {
+					// clear relays_info to fetch all replies again
+					issue.relays_info = {};
+					issue.status_history = [];
+					issue.quality_children = [];
+					issue.quality_children_count = 0;
+					issue.deleted_children_ids = [];
+					issue.status = IssueOrPrStatus.Open;
+				}),
+			tx
+				.table('prs')
+				.toCollection()
+				.modify((pr) => {
+					// clear relays_info to fetch all replies again
+					pr.relays_info = {};
+					pr.status_history = [];
+					pr.quality_children = [];
+					pr.quality_children_count = 0;
+					pr.deleted_children_ids = [];
+					pr.status = IssueOrPrStatus.Open;
+				}),
+			tx
+				.table('repos')
+				.toCollection()
+				.modify((repos) => {
+					// clear relays_info to fetch all issues and prs again
+					repos.relays_info = {};
+				})
+		]);
+		await clearLocalRelayDb();
+	});
 
 // Add hooks that will index "message" for full-text search:
 db.repos.hook('creating', function (_primKey, repo_item: RepoTableItem) {
