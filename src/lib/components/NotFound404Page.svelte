b321256764722716990755822e37ff9ef5ee23b8
DanConwayDev <DanConwayDev@protonmail.com>
2025-03-07 09:44:55 +0000

    add 404 page support for repo header

--- src/lib/components/NotFound404Page.svelte
+++ src/lib/components/NotFound404Page.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
 	import ContainerCenterPage from './ContainerCenterPage.svelte';
 
-	let { msg }: { msg?: string | undefined } = $props();
+	let {
+		msg,
+		repo_header_on_page = false
+	}: { msg?: string | undefined; repo_header_on_page?: boolean } = $props();
 
 	let use_msg = $derived(msg ? msg : 'Sorry, the page you are looking for does not exist.');
 </script>
 
-<ContainerCenterPage>
+<ContainerCenterPage {repo_header_on_page}>
 	<div class="text-center">
 		<h1 class="mb-2 text-9xl font-bold">404</h1>
 		<h3 class="mb-4 text-2xl font-bold">Not Found</h3>
