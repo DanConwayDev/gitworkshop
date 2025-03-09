8040d5ccd133a93a3f42ec229c2948058a70c2a2
DanConwayDev <DanConwayDev@protonmail.com>
2025-03-09 22:12:24 +0000

    feat(outbox): add dismiss option in ui

--- src/lib/components/NavBarInsertOutbox.svelte
+++ src/lib/components/NavBarInsertOutbox.svelte
@@ -89,18 +89,41 @@
 				</div>
 
 				{#each filtered as o}
-					<button
-						class="w-full p-2 hover:bg-base-200"
-						class:bg-base-300={selected === o.id}
-						onclick={() => {
-							if (selected === o.id) selected = undefined;
-							else selected = o.id;
-						}}
-					>
-						<div class="badge">{kindtoTextLabel(o.event.kind)}</div>
-						<span class="text-xs"><FromNow unix_seconds={o.event.created_at} /></span>
-						sent to {o.relay_logs.filter((l) => l.success).length} / {o.relay_logs.length} relays
-					</button>
+					<div class="group flex w-full items-center justify-between p-2 hover:bg-base-200">
+						<button
+							class="flex-grow cursor-pointer"
+							class:bg-base-300={selected === o.id}
+							onclick={() => {
+								if (selected === o.id) selected = undefined;
+								else selected = o.id;
+							}}
+						>
+							<div class="badge">{kindtoTextLabel(o.event.kind)}</div>
+							<span class="text-xs"><FromNow unix_seconds={o.event.created_at} /></span>
+							sent to {o.relay_logs.filter((l) => l.success).length} / {o.relay_logs.length} relays
+						</button>
+						<button
+							class="btn btn-ghost btn-xs opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+							aria-label="dismiss"
+							onclick={() => {
+								db.outbox.delete(o.id);
+							}}
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								class="h-3 w-3"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+						</button>
+					</div>
+
 					{#if selected === o.id}
 						<div>
 							{#each o.relay_logs.reduce((acc, log) => {
