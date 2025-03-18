347add2fe4d0206de0bbac605ac3767c05cd32e2
DanConwayDev <DanConwayDev@protonmail.com>
2025-03-18 14:59:09 +0000

    fix(navbar): use same menu style in user and left

--- src/lib/components/NavBarUserMenu.svelte
+++ src/lib/components/NavBarUserMenu.svelte
@@ -40,7 +40,7 @@
 	{/if}
 
 	<Sidebar bind:is_open classes="w-[500px]">
-		<div class="text-wrap">
+		<div class="w-full text-wrap">
 			{#if store.logged_in_account}
 				<div class="-mb-2">
 					<UserHeader user={store.logged_in_account.pubkey} size="full" link_to_profile={false} />
@@ -110,34 +110,74 @@
 
 				<div class="flex-grow"></div>
 			</div>
-			<ul class="-mx-4">
+			<ul class="flex w-full flex-col gap-1 py-2">
 				{#if store.experimental}
-					<li>
+					<li class="w-full overflow-hidden rounded-md">
 						<button
-							class="h-10 w-full px-8 text-right hover:bg-base-200 active:bg-base-100"
+							class="flex h-10 w-full items-center justify-end px-4 transition-colors hover:bg-base-200 active:bg-base-100"
 							onclick={() => {
 								show_feedback_modal = true;
-							}}>Feedback</button
+							}}
 						>
+							<span class="mr-2">Feedback</span>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								class="h-5 w-5"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+						</button>
 					</li>
 				{/if}
-				<li>
+				<li class="w-full overflow-hidden rounded-md">
 					<button
-						class="h-10 w-full px-8 text-right hover:bg-base-200 active:bg-base-100"
+						class="flex h-10 w-full items-center justify-end px-4 transition-colors hover:bg-base-200 active:bg-base-100"
 						onclick={() => {
 							show_settings_modal = true;
 						}}
 					>
-						Settings
+						<span class="mr-2">Settings</span>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-5 w-5"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
+								clip-rule="evenodd"
+							/>
+						</svg>
 					</button>
 				</li>
-				<li>
+				<li class="w-full overflow-hidden rounded-md">
 					<button
-						class="h-10 w-full px-8 text-right hover:bg-base-200 active:bg-base-100"
+						class="flex h-10 w-full items-center justify-end px-4 transition-colors hover:bg-base-200 active:bg-base-100"
 						onclick={() => {
 							accounts_manager.clearActive();
-						}}>Logout</button
+						}}
 					>
+						<span class="mr-2">Logout</span>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-5 w-5"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M3 3a1 1 0 00-1 1v12a1 1 0 001 1h12a1 1 0 001-1V4a1 1 0 00-1-1H3zm6 4a1 1 0 011 1v4.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 111.414-1.414L8 12.586V8a1 1 0 011-1z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+					</button>
 				</li>
 			</ul>
 		</div>
