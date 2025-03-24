f155696514dc6774cec6092ddcde4bb1af844a6b
DanConwayDev <DanConwayDev@protonmail.com>
2025-03-24 17:07:36 +0000

    feat(navbar): add search button
    closes note1jfgmt0az42l3lpkdrwxkgfp88ee5374exevel8x2ssmld90q0exsdn6h6h

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -33,26 +33,30 @@
 			</div>
 			<div class="navbar-end md:gap-2">
 				<form onsubmit={handleSearch} class="mx-2 hidden sm:flex">
-					<label class="input input-sm input-bordered flex items-center gap-2">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 16 16"
-							fill="currentColor"
-							class="h-4 w-4 opacity-70"
-						>
-							<path
-								fill-rule="evenodd"
-								d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-								clip-rule="evenodd"
+					<div class="join">
+						<label class="input input-sm join-item input-bordered flex items-center gap-2">
+							<input
+								type="text"
+								class="grow"
+								placeholder="Find Repo by Name"
+								bind:value={search_input}
 							/>
-						</svg>
-						<input
-							type="text"
-							class="grow"
-							placeholder="Find Repo by Name"
-							bind:value={search_input}
-						/>
-					</label>
+						</label>
+						<button type="submit" aria-label="search" class="btn join-item input-bordered btn-sm">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 16 16"
+								fill="currentColor"
+								class="h-4 w-4 opacity-70"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+						</button>
+					</div>
 				</form>
 				{#if store.experimental}<NavBarInsertWallet />{/if}
 				{#if store.experimental}<NavBarInsertOutbox />{/if}
