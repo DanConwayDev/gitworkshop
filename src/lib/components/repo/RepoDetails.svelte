1f14f7eaa6caa816094b5b348359309e1a6bc21e
DanConwayDev <DanConwayDev@protonmail.com>
2025-04-03 12:23:49 +0100

    feat(RepoDetails): include git nostr url directly
    instead of behind `clone` button

--- src/lib/components/repo/RepoDetails.svelte
+++ src/lib/components/repo/RepoDetails.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { icons_misc } from '$lib/icons';
-	import InstallNgit from '$lib/components//InstallNgit.svelte';
 	import {
 		repoTableItemDefaults,
 		type RepoRef,
@@ -62,73 +61,62 @@
 			<p class="my-2 break-words text-sm">{item.identifier}</p>
 		{/if}
 	{/if}
-	<div class="dropdown dropdown-end mt-3">
-		<div tabIndex={0} class="btn btn-success btn-sm text-base-400">
-			clone
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 24 24"
-				class="h-5 w-5 flex-none fill-success-content"
-				><path
-					fill="currentColor"
-					d="M11.646 15.146L5.854 9.354a.5.5 0 0 1 .353-.854h11.586a.5.5 0 0 1 .353.854l-5.793 5.792a.5.5 0 0 1-.707 0"
-				/></svg
-			>
-		</div>
-		<ul
-			tabIndex={0}
-			class="w-md menu dropdown-content z-[1] ml-0 rounded-box bg-base-300 p-2 shadow"
-		>
-			<li class="prose">
-				<div>
-					<div>
-						<h4 class="mt-0">1. install ngit and git-remote-nostr</h4>
-						<InstallNgit size="sm" />
-					</div>
-				</div>
-			</li>
-			<li class="m-0 p-0">
-				<!-- eslint-disable-next-line svelte/valid-compile -->
-				<!-- svelte-ignore a11y_click_events_have_key_events -->
-				<!-- svelte-ignore a11y_no_static_element_interactions -->
-				<div
-					onclick={async () => {
-						try {
-							await navigator.clipboard.writeText(nostr_url);
-							nostr_url_copied = true;
-							setTimeout(() => {
-								nostr_url_copied = false;
-							}, 2000);
-						} catch {
-							/* empty */
-						}
-					}}
-					class="group cursor-pointer rounded-md"
-				>
-					<div>
-						<h4 class="mt-0 pt-0">
-							2. copy git clone url
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 16 16"
-								class="ml-1 inline h-4 w-4 flex-none fill-base-content opacity-50 group-hover:opacity-100"
-								class:fill-base-content={!nostr_url_copied}
-								class:fill-success={nostr_url_copied}
-							>
-								{#each icons_misc.copy as d}
-									<path {d} />
-								{/each}
-							</svg>
 
-							{#if nostr_url_copied}<span class="text-sm text-success opacity-50">
-									(copied to clipboard)</span
-								>{/if}
-						</h4>
-						<p class="my-2 break-words border p-2 text-xs">{nostr_url}</p>
-					</div>
+	<div>
+		{#if repo_not_found}<div></div>
+		{:else if !item || loading}
+			<div class="skeleton my-3 h-5 w-20"></div>
+			<div class="badge skeleton my-2 block w-60"></div>
+		{:else}
+			<h4>
+				nostr clone url {#if git_url_copied}<span class="text-sm text-success opacity-50">
+						(copied to clipboard)</span
+					>{/if}
+			</h4>
+			<!-- eslint-disable-next-line svelte/valid-compile -->
+			<!-- svelte-ignore a11y_click_events_have_key_events -->
+			<!-- svelte-ignore a11y_no_static_element_interactions -->
+			<div
+				onclick={async () => {
+					try {
+						await navigator.clipboard.writeText(nostr_url);
+						nostr_url_copied = true;
+						setTimeout(() => {
+							nostr_url_copied = false;
+						}, 2000);
+					} catch {
+						/* empty */
+					}
+				}}
+				class="group my-2 mt-3 cursor-pointer break-words text-sm"
+				class:text-success={nostr_url_copied}
+				class:opacity-50={nostr_url_copied}
+			>
+				{nostr_url}
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 16 16"
+					class="ml-1 inline h-4 w-4 flex-none fill-base-content opacity-50"
+					class:group-hover:opacity-100={!nostr_url_copied}
+					class:fill-base-content={!nostr_url_copied}
+					class:fill-success={nostr_url_copied}
+					class:opacity-100={nostr_url_copied}
+				>
+					{#each icons_misc.copy as d}
+						<path {d} />
+					{/each}
+				</svg>
+			</div>
+
+			<div class="inline-block bg-base-300 p-2">
+				<span class="opacity-60">
+					just <a href="/ngit" class="link-primary">install ngit</a> and run</span
+				>
+				<div class="inline-block rounded-md bg-base-400 p-2 font-mono text-sm">
+					git clone nostr://...
 				</div>
-			</li>
-		</ul>
+			</div>
+		{/if}
 	</div>
 
 	<div>
