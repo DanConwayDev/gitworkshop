2cd003bf71f937975716676e5082b8be751b1ba5
DanConwayDev <DanConwayDev@protonmail.com>
2025-04-18 08:53:38 +0100

    feat(NavBar): add search link on mobile

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -58,6 +58,26 @@
 						</button>
 					</div>
 				</form>
+				<div class="relative sm:hidden">
+					<a
+						href="/search"
+						aria-label="search"
+						class="btn btn-ghost btn-sm mt-1 h-6 px-2 pb-1 pt-1"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 16 16"
+							fill="currentColor"
+							class="h-6 w-6"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+					</a>
+				</div>
 				{#if store.experimental}<NavBarInsertWallet />{/if}
 				{#if store.experimental}<NavBarInsertOutbox />{/if}
 
