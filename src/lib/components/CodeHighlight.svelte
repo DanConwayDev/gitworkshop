085d8783742174898dddd4ee4f07d9454915d2f3
DanConwayDev <DanConwayDev@protonmail.com>
2025-06-27 16:31:12 +0100

    add code highlighting

---
+++ src/lib/components/CodeHighlight.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { HighlightAuto, LineNumbers } from 'svelte-highlight';
+	import github from 'svelte-highlight/styles/github-dark';
+	// Props
+	let { content, path } = $props();
+
+	// Derive language from file path
+	// let language = $derived(getLanguageFromPath(path));
+</script>
+
+<svelte:head>
+	{@html github}
+</svelte:head>
+
+<HighlightAuto code={content} let:highlighted>
+	<LineNumbers
+		--border-color="var(--fallback-b3, oklch(var(--b3) / var(--tw-bg-opacity, 1)))0"
+		--line-number-color="#8b949e"
+		{highlighted}
+	/>
+</HighlightAuto>
+
+<style>
+	:global(.hljs) {
+		background: transparent !important;
+		padding: 1rem;
+		margin: 0;
+	}
+</style>
--- src/lib/components/explorer/FileViewer.svelte
+++ src/lib/components/explorer/FileViewer.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { stringToDocTree } from '$lib/doc_tree';
 	import store from '$lib/store.svelte';
+	import CodeHighlight from '../CodeHighlight.svelte';
 	import ContentTree from '../content-tree/ContentTree.svelte';
 
 	let { path, content }: { path: string; content?: string } = $props();
@@ -10,22 +11,24 @@
 	<div class="border-b border-base-400 bg-base-300 px-6 py-3">
 		<h4 class="">{path}</h4>
 	</div>
-	<div class="p-6">
-		{#if !content}
+	{#if !content}
+		<div class="p-6">
 			<div class="skeleton my-3 h-5 w-20"></div>
 			<div class="skeleton my-2 h-4"></div>
 			<div class="skeleton my-2 mb-3 h-4 w-2/3"></div>
 			<div class="skeleton my-3 h-5 w-20"></div>
 			<div class="skeleton my-2 h-4"></div>
 			<div class="skeleton my-2 mb-3 h-4 w-2/3"></div>
-		{:else}
+		</div>
+	{:else if path.toLowerCase().endsWith('.md')}
+		<div class="p-6">
 			<article class="">
-				{#if path.toLowerCase().endsWith('.md')}
-					<ContentTree node={stringToDocTree(content)} />
-				{:else}
-					{content}
-				{/if}
+				<ContentTree node={stringToDocTree(content)} />
 			</article>
-		{/if}
-	</div>
+		</div>
+	{:else}
+		<article class="">
+			<CodeHighlight {content} {path} />
+		</article>
+	{/if}
 </div>
