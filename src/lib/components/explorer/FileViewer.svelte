15a4263d0b49efce06edb89d570e54913de952e8
DanConwayDev <DanConwayDev@protonmail.com>
2025-07-03 15:08:41 +0100

    explorer: allow viewing md raw

--- src/lib/components/explorer/FileViewer.svelte
+++ src/lib/components/explorer/FileViewer.svelte
@@ -5,11 +5,18 @@
 	import ContentTree from '../content-tree/ContentTree.svelte';
 
 	let { path, content }: { path: string; content?: string } = $props();
+	let use_markdown = $state(true);
 </script>
 
 <div class="my-3 rounded-lg border border-base-400">
-	<div class="border-b border-base-400 bg-base-300 px-6 py-3">
-		<h4 class="">{path}</h4>
+	<div class="border-b border-base-400 bg-base-300 flex">
+		<div class="px-6 py-3 flex-grow"><h4 class="">{path}</h4></div>
+		{#if path.toLowerCase().endsWith('.md')}
+			<div class="tabs tabs-lifted tabs-lg">
+				<button class="tab text-sm" class:tab-active={!use_markdown} onclick={()=> {use_markdown = false}}>Raw</button>
+				<button class="tab text-sm" class:tab-active={use_markdown} onclick={()=> {use_markdown = true}}>Markdown</button>
+			</div>
+		{/if}
 	</div>
 	{#if !content}
 		<div class="p-6">
@@ -20,7 +27,7 @@
 			<div class="skeleton my-2 h-4"></div>
 			<div class="skeleton my-2 mb-3 h-4 w-2/3"></div>
 		</div>
-	{:else if path.toLowerCase().endsWith('.md')}
+	{:else if path.toLowerCase().endsWith('.md') && use_markdown}
 		<div class="p-6">
 			<article class="">
 				<ContentTree node={stringToDocTree(content)} />
