DanConwayDev/gitworkshop

commit c66b38df627922702a0193a3aa92e7051cbaf1af

status: support reading nip22 style status
in preparation for its coming

src/lib/relay/RelayManager.ts
@@ -654,6 +654,7 @@ export class RelayManager {
 				filters: [
 					{
 						'#a': query_a_tags.size == 0 ? undefined : [...query_a_tags],
+						'#q': query_a_tags.size == 0 ? undefined : [...query_a_tags],
 						'#e': query_e_tags.size == 0 ? undefined : [...query_e_tags],
 						since: unixNow()
 					},

src/lib/relay/filters.ts
@@ -139,7 +139,8 @@ export const createRepoChildrenFilters = (
 		return [
 			{
 				kinds: [IssueKind, PatchKind, DeletionKind],
-				'#a': [...items]
+				'#a': [...items],
+				'#q': [...items]
 			}
 		];
 	}
@@ -154,7 +155,8 @@ export const createRepoChildrenFilters = (
 	sinces.forEach((a_refs, since) => {
 		const filter: Filter = {
 			kinds: [IssueKind, PatchKind, DeletionKind],
-			'#a': a_refs
+			'#a': a_refs,
+			'#q': a_refs
 		};
 		if (since > 0) {
 			filter.since = since;

commit 4e8fddd07e0e7b44b1fe387b56efa9dbde157076

fix:filters causing no issues or pr
broken by c66b38df627922702a0193a3aa92e7051cbaf1af

q filter isn't really needed anyway

src/lib/relay/RelayManager.ts
@@ -654,7 +654,10 @@ export class RelayManager {
 				filters: [
 					{
 						'#a': query_a_tags.size == 0 ? undefined : [...query_a_tags],
-						'#q': query_a_tags.size == 0 ? undefined : [...query_a_tags],
+						since: unixNow()
+					},
+
+					{
 						'#e': query_e_tags.size == 0 ? undefined : [...query_e_tags],
 						since: unixNow()
 					},

src/lib/relay/filters.ts
@@ -139,8 +139,7 @@ export const createRepoChildrenFilters = (
 		return [
 			{
 				kinds: [IssueKind, PatchKind, DeletionKind],
-				'#a': [...items],
-				'#q': [...items]
+				'#a': [...items]
 			}
 		];
 	}
@@ -155,8 +154,7 @@ export const createRepoChildrenFilters = (
 	sinces.forEach((a_refs, since) => {
 		const filter: Filter = {
 			kinds: [IssueKind, PatchKind, DeletionKind],
-			'#a': a_refs,
-			'#q': a_refs
+			'#a': a_refs
 		};
 		if (since > 0) {
 			filter.since = since;