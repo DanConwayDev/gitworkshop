d921f68ef16c58f0c3f26e6b757569077e46ef7f
DanConwayDev <DanConwayDev@protonmail.com>
2025-08-19 16:10:46 +0100

    remove experimental feedback
    as Im not monitoring this kind anymore

--- src/lib/components/NavBarUserMenu.svelte
+++ src/lib/components/NavBarUserMenu.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import accounts_manager from '$lib/accounts';
 	import store from '$lib/store.svelte';
-	import FeedbackModal from './FeedbackModal.svelte';
 	import LoginModal from './LoginModal.svelte';
 	import SettingsModal from './SettingsModal.svelte';
 	import Sidebar from './Sidebar.svelte';
@@ -16,7 +15,6 @@
 		store.navbar_fixed = is_open;
 	};
 	let show_login_modal = $state(false);
-	let show_feedback_modal = $state(false);
 	let show_settings_modal = $state(false);
 
 	let repos_query = $derived(
@@ -157,30 +155,6 @@
 				<div class="grow"></div>
 			</div>
 			<ul class="flex w-full flex-col gap-1 py-2">
-				{#if store.experimental}
-					<li class="w-full overflow-hidden rounded-md">
-						<button
-							class="hover:bg-base-200 active:bg-base-100 flex h-10 w-full items-center justify-end px-4 transition-colors"
-							onclick={() => {
-								show_feedback_modal = true;
-							}}
-						>
-							<span class="mr-2">Feedback</span>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class="h-5 w-5"
-								viewBox="0 0 20 20"
-								fill="currentColor"
-							>
-								<path
-									fill-rule="evenodd"
-									d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z"
-									clip-rule="evenodd"
-								/>
-							</svg>
-						</button>
-					</li>
-				{/if}
 				<li class="w-full overflow-hidden rounded-md">
 					<button
 						class="hover:bg-base-200 active:bg-base-100 flex h-10 w-full items-center justify-end px-4 transition-colors"
@@ -229,14 +203,6 @@
 		</div>
 	</Sidebar>
 </div>
-
-{#if show_feedback_modal}
-	<FeedbackModal
-		done={() => {
-			show_feedback_modal = false;
-		}}
-	/>
-{/if}
 
 {#if show_settings_modal}
 	<SettingsModal
