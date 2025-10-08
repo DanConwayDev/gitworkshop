91c5f2c90d39a50c7d64db8955544f6ac191f966
DanConwayDev <DanConwayDev@protonmail.com>
2025-10-08 15:25:33 +0100

    fix(pr): PR update hides commits from original PR or earlier updates

--- src/lib/components/event/PullRequestUpdate.svelte
+++ src/lib/components/event/PullRequestUpdate.svelte
@@ -2,30 +2,40 @@
 	import { type NostrEvent } from 'nostr-tools';
 	import type { IssueOrPRTableItem, PubKeyString } from '$lib/types';
 	import EventWrapperLite from './EventWrapperLite.svelte';
-	import { liveQueryState } from '$lib/helpers.svelte';
+	import { inMemoryRelayTimeline, liveQueryState } from '$lib/helpers.svelte';
 	import db from '$lib/dbs/LocalDb';
 	import { getTagValue } from '$lib/utils';
 	import git_manager from '$lib/git-manager';
 	import type { CommitInfo } from '$lib/types/git-manager';
 	import CommitOneLineSummaries from '../prs/CommitOneLineSummaries.svelte';
 	import { onMount } from 'svelte';
+	import { PrUpdateKind } from '$lib/kinds';
+	import query_centre from '$lib/query-centre/QueryCentre.svelte';
 	let {
 		event,
 		issue_or_pr_table_item
 	}: { event: NostrEvent; issue_or_pr_table_item?: IssueOrPRTableItem } = $props();
 
+	let pr_event_id: string | undefined = $derived(getTagValue(event.tags, 'E'));
+
+	let pr_table_item_query = $derived(pr_event_id ? query_centre.fetchPr(pr_event_id) : undefined);
+	let pr_table_item = $derived(pr_table_item_query ? pr_table_item_query.current : undefined);
+
+	let pr_repos = $derived(pr_table_item?.repos ?? []);
+	let pr_author = $derived(pr_table_item?.author);
 	let item_maintainers_query = $derived(
-		liveQueryState(async () => {
-			let a_refs = issue_or_pr_table_item?.repos ?? [];
-			let items = await db.repos.bulkGet(a_refs);
-			let maintainers: PubKeyString[] = [];
-			items.forEach((item) => item?.maintainers?.forEach((m) => maintainers.push(m)));
-			return [issue_or_pr_table_item?.author, ...maintainers];
-		})
+		liveQueryState(
+			async () => {
+				let a_refs = pr_repos;
+				let items = await db.repos.bulkGet(a_refs);
+				let maintainers: PubKeyString[] = [];
+				items.forEach((item) => item?.maintainers?.forEach((m) => maintainers.push(m)));
+				return [...(pr_author ? [pr_author] : []), ...maintainers];
+			},
+			() => [pr_repos, pr_author]
+		)
 	);
-	let item_maintainers = $derived(
-		item_maintainers_query.current ?? [issue_or_pr_table_item?.author]
-	);
+	let item_maintainers = $derived(item_maintainers_query.current ?? []);
 	let with_permission = $derived(item_maintainers.includes(event.pubkey));
 
 	let tip_id = $derived(getTagValue(event.tags, 'c') || '[commit_id unknown]');
@@ -34,7 +44,7 @@
 		event.tags.flatMap((s) => (s[0] === 'a' && s[1] !== undefined ? [s[1]] : []))
 	);
 
-	let commits: CommitInfo[] | undefined = $state();
+	let commits_on_branch: CommitInfo[] | undefined = $state();
 	let interval_id = $state<number | undefined>();
 	let loading: boolean = $state(true);
 	const loadCommitInfos = async (event_id: string, tip_id: string) => {
@@ -44,7 +54,7 @@
 				$state.snapshot(event_id),
 				$state.snapshot(tip_id)
 			);
-			if (infos) commits = infos;
+			if (infos) commits_on_branch = infos;
 			loading = false;
 		} else {
 			interval_id = setInterval(() => {
@@ -53,6 +63,29 @@
 		}
 	};
 
+	let previous_tip_query = $derived(
+		pr_event_id
+			? inMemoryRelayTimeline([
+					{ ids: [pr_event_id] },
+					{ kinds: [PrUpdateKind], '#E': [pr_event_id] }
+				])
+			: { timeline: [] }
+	);
+	let previous_tip = $derived(
+		// the PR event may not be in local relay so supliment with pr_table_item.event
+		[...(pr_table_item ? [pr_table_item.event] : []), ...previous_tip_query.timeline]
+			.filter((e) => item_maintainers.includes(e.pubkey) && e.created_at < event.created_at)
+			.sort((a, b) => a.created_at - b.created_at)
+			.map((e) => getTagValue(e.tags, 'c'))
+			.find((e) => !!e)
+	);
+
+	let new_commits = $derived.by(() => {
+		if (!commits_on_branch || !previous_tip) return commits_on_branch;
+		const idx = commits_on_branch.findIndex((c) => c && c.oid === previous_tip);
+		return idx === -1 ? commits_on_branch : commits_on_branch.slice(idx + 1);
+	});
+
 	onMount(() => {
 		loadCommitInfos(event.id, tip_id);
 	});
@@ -60,10 +93,9 @@
 
 {#snippet commitInfos()}
 	<div class="md:ml-10">
-		<CommitOneLineSummaries infos={commits} {loading} />
+		<CommitOneLineSummaries infos={new_commits} {loading} />
 	</div>
 {/snippet}
-
 <EventWrapperLite {event} name_first children_below={commitInfos}>
 	<span class="text-sm">
 		{#if with_permission}pushed PR updates
