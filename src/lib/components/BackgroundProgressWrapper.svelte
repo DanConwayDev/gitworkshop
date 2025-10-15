e38e2fbfb92666d72b90db323013cb2fe3d03edd
DanConwayDev <DanConwayDev@protonmail.com>
2025-10-15 07:06:38 +0100

    fix: progress background suppressing dropdown behaviour

--- src/lib/components/BackgroundProgressWrapper.svelte
+++ src/lib/components/BackgroundProgressWrapper.svelte
@@ -11,7 +11,7 @@
 	} = $props();
 </script>
 
-<div class="relative w-full overflow-hidden rounded">
+<div class="relative w-full rounded">
 	<div
 		class="{complete_bg_color_class} absolute top-0 bottom-0 left-0 transition-all duration-300"
 		style="width: {Math.min(pc, 100)}%;"
