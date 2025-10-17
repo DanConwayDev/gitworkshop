239f1fe8985a22bdf3623fc164dd937cf87842f1
DanConwayDev <DanConwayDev@protonmail.com>
2025-10-17 07:13:39 +0100

    fix(git): CommitsDetails lite view off by default

--- src/lib/components/prs/CommitsDetails.svelte
+++ src/lib/components/prs/CommitsDetails.svelte
@@ -91,7 +91,7 @@
 {/snippet}
 
 {#snippet showItem(info: CommitInfo)}
-	{#if !lite_view}
+	{#if lite_view}
 		<div
 			class="border-base-400 bg-base-200 bg-base-100 flex items-start gap-3 border-x px-3 py-3 sm:flex-row sm:items-center sm:gap-4"
 			class:border-t={false}
@@ -134,32 +134,6 @@
 						<FromNow unix_seconds={info.committer.timestamp} />
 					</div>
 				</div>
-			</div>
-		</div>
-	{:else if true}
-		<div class="bg-base-200 my-2 flex items-center gap-2 rounded p-2">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 16 16"
-				class="text-base-content h-4 w-4 flex-none"
-			>
-				<title>Commit</title>
-				<path fill="currentColor" d={pr_icon_path.commit} />
-			</svg>
-
-			<div class="min-w-0 flex-1">
-				<div class="flex items-center gap-2">
-					<div class="flex-grow truncate font-mono text-sm font-medium">
-						{info.message.split(/[\r\n]/)[0]}
-					</div>
-					{#if info.author.name}
-						<div class="text-base-content/50 shrink-0 text-xs">{info.author.name}</div>
-					{/if}
-				</div>
-			</div>
-
-			<div class="commit-id text-base-content/40 ml-2 flex-shrink-0 text-xs">
-				{info.oid.substring(0, 8)}
 			</div>
 		</div>
 	{:else}
