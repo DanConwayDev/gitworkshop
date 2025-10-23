03040ac85747e0050ac21b41b66106c28f92c0b6
DanConwayDev <DanConwayDev@protonmail.com>
2025-10-23 21:31:47 +0100

    fix: make offline banner a more subtle color

--- src/lib/components/OfflineBanner.svelte
+++ src/lib/components/OfflineBanner.svelte
@@ -3,7 +3,7 @@
 	let { msg } = $props();
 </script>
 
-<div class="border-accent-content bg-error text-accent-content border-b">
+<div class="border-accent-content bg-subtle-error text-subtle-error-content border-b">
 	<Container no_wrap={true}>
 		<div class="flex items-center justify-between p-3 py-1">
 			<div class="flex-1 text-left text-xs font-semibold">Offline Mode</div>
