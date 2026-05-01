4904938809946ad1971aab86b20538ef5d173a6c
DanConwayDev <DanConwayDev@protonmail.com>
2026-05-01 21:52:44 +0100

    bump ngit 2.4.3

--- src/pages/NgitPage.tsx
+++ src/pages/NgitPage.tsx
@@ -15,7 +15,7 @@ import { Copy, Check, Download, Terminal, Package, Wrench } from "lucide-react";
 // Constants
 // ---------------------------------------------------------------------------
 
-const NGIT_VERSION = "v2.4.2";
+const NGIT_VERSION = "v2.4.3";
 
 // ---------------------------------------------------------------------------
 // Platform detection
